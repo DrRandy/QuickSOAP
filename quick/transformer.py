e173f2f457f3308518389ba688665ba9c6aec7e0
DrRandy <DrRandy@users.noreply.github.com>
2023-12-31 14:43:09 -0500

    Update transformer.py: first and second pass transformation now working

--- quick/transformer.py
+++ quick/transformer.py
@@ -3,9 +3,22 @@ from lark import Transformer
 
 def transform(ast):
     output = QuickTransformer().transform(ast)
-    # token_list = TokenListTransformer().transform(ast)
-    return output
+    token_dict = TokenListTransformer().transform(ast)
+    result = insert_tokens(output, token_dict)
+    return result
 
+def insert_tokens(the_string, the_tokens):
+    output = the_string
+    # first pass - replace only the first occurrence of the token
+    for the_key, the_value in the_tokens.items():
+        the_token = "<<<" + the_key + ">>>"
+        output = output.replace(the_token, the_value['firstpass'], 1)
+    # second pass - replace all subsequent occurrences of the token
+    for the_key, the_value in the_tokens.items():
+        the_token = "<<<" + the_key + ">>>"
+        output = output.replace(the_token, the_value['secondpass'])
+    return output
+    
 
 class QuickTransformer(Transformer):
 
@@ -44,7 +57,7 @@ class QuickTransformer(Transformer):
     
     def textbox_named(self, token):
         name = token[0]
-        result = '[text name="' + name + '"]'
+        result = '<<<' + name + '>>>'
         return result
     
     def textbox_named_default_text(self, token):
@@ -59,7 +72,7 @@ class QuickTransformer(Transformer):
     
     def dropdown_named_options(self, token):
         name, options = token
-        result = '[text name="' + name + '" value=' + default_text + ']'
+        result = '<<<' + name + '>>>'
         return result
     
     def checkbox_anonymous(self, token):
@@ -73,12 +86,12 @@ class QuickTransformer(Transformer):
     
     def checkbox_named(self, token):
         name = token[0]
-        result = '[checkbox name="' + name + '"]'
+        result = '<<<' + name + '>>>'
         return result
     
     def checkbox_named_caption(self, token):
         name, caption = token
-        result = '[checkbox name="' + name + '" value=' + caption + ']'
+        result = '<<<' + name + '>>>'
         return result
     
     def default_text(self, token):
@@ -93,6 +106,88 @@ class QuickTransformer(Transformer):
         escaped_string = token[0]
         return escaped_string
 
+
+class TokenListTransformer(Transformer):
+
+    def value(self, token):
+        token_dict = dict()
+        for item in token:
+            if not item==[]:
+                the_key, the_value = item[0]
+                if not the_key in token_dict:
+                    token_dict[the_key] = the_value
+        return token_dict
+        
+    def chunk(self, token): 
+        return token
+
+    # def textbox_chunk(self, token): # not needed, default is to return the token
+
+    # def dropdown_chunk(self, token): # not needed, default is to return the token
+
+    # def checkbox_chunk(self, token): # not needed, default is to return the token
+    
+    def string_chunk(self, token):
+        return lark.visitors.Discard
+    
+    def textbox_anonymous(self, token):
+        return lark.visitors.Discard 
+    
+    def textbox_anonymous_default_text(self, token):
+        return lark.visitors.Discard
+    
+    def textbox_named(self, token):
+        name = token[0]
+        firstpass = '[text name="' + name + '"]'
+        secondpass = '[var name="' + name + '"]'
+        result = name+"", {'firstpass': firstpass, 'secondpass': secondpass}
+        return result
+    
+    def textbox_named_default_text(self, token):
+        name, default_text = token
+        firstpass = '[text name="' + name + '" value="' + default_text + '"]'
+        secondpass = '[var name="' + name + '"]'
+        result = name+"", {'firstpass': firstpass, 'secondpass': secondpass}
+        return result
+    
+    def dropdown_anonymous_options(self, token):
+        return lark.visitors.Discard
+    
+    def dropdown_named_options(self, token):
+        name, options = token
+        firstpass = '[select name="' + name + '" value=' + default_text + ']'
+        secondpass = '[var name="' + name + '"]'
+        result = name+"", {'firstpass': firstpass, 'secondpass': secondpass}
+        return result
+    
+    def checkbox_anonymous(self, token):
+        return lark.visitors.Discard
+    
+    def checkbox_anonymous_caption(self, token):
+        return lark.visitors.Discard
+    
+    def checkbox_named(self, token):
+        name = token[0]
+        firstpass = '[checkbox name="' + name + '"]'
+        secondpass = '[var name="' + name + '"]'
+        result = name+"", {'firstpass': firstpass, 'secondpass': secondpass}
+        return result
+    
+    def checkbox_named_caption(self, token):
+        name, caption = token
+        firstpass = '[checkbox name="' + name + '" value=' + caption + ']'
+        secondpass = '[var name="' + name + '"]'
+        result = name+"", {'firstpass': firstpass, 'secondpass': secondpass}
+        return result
+    
+    def default_text(self, token):
+        return token[0]
+                         
+    def dropdown_options(self, token):
+        return token[0]
+                      
+    def caption(self, token):
+        return token[0]
 
 """
 class TokenListTransformer(Transformer):
