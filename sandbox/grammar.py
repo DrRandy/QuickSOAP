6c32bc5ef119cc00e86dbc2abb98e5a2aad9a39f
DrRandy <DrRandy@users.noreply.github.com>
2024-01-01 21:01:20 +0000

    generate grammar from csv file

--- sandbox/grammar.py
+++ sandbox/grammar.py
@@ -1,29 +1,28 @@
-
 quickgrammar = r"""
 
     value   : chunk+
     
     chunk   : textbox_chunk 
-	|  dropdown_chunk 
-	|  checkbox_chunk 
-	|  string_chunk 
+        |  dropdown_chunk 
+        |  checkbox_chunk 
+        |  string_chunk 
     
     textbox_chunk   : "`@" -> textbox_anonymous 
-	|  "`@=" default_text -> textbox_anonymous_default_text 
-	|  "`@" WORD -> textbox_named
-	|  "`@" WORD "=" default_text -> textbox_named_default_text
+        |  "`@=" default_text -> textbox_anonymous_default_text 
+        |  "`@" WORD -> textbox_named
+        |  "`@" WORD "=" default_text -> textbox_named_default_text
     
     dropdown_chunk   : "`^=" dropdown_options -> dropdown_anonymous_options 
-	|  "`^" WORD -> dropdown_named 
-	|  "`^" WORD "=" dropdown_options -> dropdown_named_options
+        |  "`^" WORD -> dropdown_named 
+        |  "`^" WORD "=" dropdown_options -> dropdown_named_options
     
     checkbox_chunk   : "`+" -> checkbox_anonymous 
-	|  "`+=" caption -> checkbox_anonymous_caption 
-	|  "`+" WORD -> checkbox_named 
-	|  "`+" WORD "=" caption -> checkbox_named_caption
+        |  "`+=" caption -> checkbox_anonymous_caption 
+        |  "`+" WORD -> checkbox_named 
+        |  "`+" WORD "=" caption -> checkbox_named_caption
     
     string_chunk   : STRING 
-	|  ESCAPED_STRING
+        |  ESCAPED_STRING
     
     default_text   : ESCAPED_STRING
     
